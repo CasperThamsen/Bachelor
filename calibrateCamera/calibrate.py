f50cc1d4abe582842606b310f13205b16707cf1a
Thamsen <casper@thamsen.dk>
2025-04-22 11:06:12 +0200

    Update .gitignore and modify calibration image paths; add calibration data files

--- calibrateCamera/calibrate.py
+++ calibrateCamera/calibrate.py
@@ -19,7 +19,7 @@ objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
  
 # Extracting path of individual image stored in a given directory
-images = glob.glob('Bachelor/calibrateCamera/ccalphotos/webcam/*.jpg')
+images = glob.glob('calibrateCamera/ccalphotos/phone/*.jpg')
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -41,8 +41,10 @@ for fname in images:
  
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-     
-    cv2.imshow('img',img)
+    #resize
+
+    resized_img = cv2.resize(img,(1920,1080))
+    cv2.imshow('img',resized_img)
     cv2.waitKey(0)
  
 cv2.destroyAllWindows()
