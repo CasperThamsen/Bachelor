0263ce033ff572d53eded1ca6aa071bda2c9cdf2
Thamsen <casper@thamsen.dk>
2025-04-28 16:37:43 +0200

    Visual feedback on data

--- kode/datacomp.py
+++ kode/datacomp.py
@@ -11,16 +11,6 @@ pose_start_time = 63
 opti_start_time = 1744193042.0
 duration_of_video = 1450
 
-fig, ax = plt.subplots()
-ax.plot(rot1opti[:,0], rot1opti[:,1], '.y')
-ax.plot(rot1opti[:,0], rot1opti[:,2], '.c')
-ax.plot(rot1opti[:,0], rot1opti[:,3], '.m')
-ax.plot(rot1opti[:,0], rot1opti[:,4], '.r')
-ax.plot(rot1opti[:,0], rot1opti[:,5], '.b')
-ax.plot(rot1opti[:,0], rot1opti[:,6], '.g')
-plt.show()
-
-
 i = 0
 index = pose_start_time
 last_time = None
@@ -46,7 +36,41 @@ rot1pose = rot1pose[rot1pose[:,0] <= 1450]
 rot1pose = rot1pose[rot1pose[:,0] >= 63]
 rot1pose = rot1pose[rot1pose[:,0].argsort()]
 
-rot1opticsv = np.savetxt("thisisatest.csv", rot1opti, delimiter=",",fmt='%.7f')
-rot1posecsv = np.savetxt("thisisatestpose.csv", rot1pose, delimiter=",",fmt='%.7f')
+rot1opticsv = np.savetxt("optitrackdata.csv", rot1opti, delimiter=",",fmt='%.7f')
+rot1posecsv = np.savetxt("posedata.csv", rot1pose, delimiter=",",fmt='%.7f')
+rot1optiLoc2 = "optitrackdata.csv"
+rot1poseLoc2 = "posedata.csv"
+
+optival = np.loadtxt(rot1optiLoc2, delimiter=',')
+poseval = np.loadtxt(rot1poseLoc2, delimiter=',')
+
+
+
+
+
+# fig1, ax1 = plt.subplots()
+# fig2, ax2 = plt.subplots()
+# #compare the data sets visually (maybe matching?)
+# #for x,y,z
+# ax1.plot(optival[:,0], optival[:,1], '.r')
+# ax1.plot(optival[:,0], optival[:,2], '.y')
+# ax1.plot(optival[:,0], optival[:,3], '.b')
+# ax1.plot(optival[:,0], optival[:,4], '.c')
+# ax1.plot(optival[:,0], optival[:,5], '.m')
+# ax1.plot(optival[:,0], optival[:,6], '.g')
+# ax1.set_title("Optitrack data")
+# ax2.plot(poseval[:,0], poseval[:,1], '.r')
+# ax2.plot(poseval[:,0], poseval[:,2], '.y')
+# ax2.plot(poseval[:,0], poseval[:,3], '.b')
+# ax2.plot(poseval[:,0], poseval[:,4], '.c')
+# ax2.plot(poseval[:,0], poseval[:,5], '.m')
+# ax2.plot(poseval[:,0], poseval[:,6], '.g')
+# ax2.set_title("Pose data")
+# ax1.set_xlabel("Frame number")
+# ax1.set_ylabel("Position (m)")
+# ax2.set_xlabel("Frame number")
+# ax2.set_ylabel("Position (m)")
+# plt.show()
+
 
 
