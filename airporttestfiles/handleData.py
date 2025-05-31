44dec4051588ada19e8686789871280322866db2
Thamsen <casper@thamsen.dk>
2025-05-31 15:07:50 +0200

    Renamed from handleData2 to handleData

--- airporttestfiles/handleData.py
+++ airporttestfiles/handleData.py
@@ -3,28 +3,19 @@ import numpy as np
 import csv
 import cv2
 
-#this file is for handling experiment files which are all from the first visit to the airport
-#They are formatted differently from the second visit.
-#time(unix),X,Y,Z,X,Y,Z,X,Y,Z,X,Y,Z
 
+#this file is for handling experiment files which are all from the seconds visit to the airport
+#They are formatted differently from the first visit.
+#Frame,Time (Seconds),RX,RZ,RY,X,Y,Z,RX,RZ,RY,X,Y,Z
 
-file_name = "experiment_004"
+file_name = "experiment_006"
 
 with open(f"airporttestfiles/{file_name}.csv", "r") as opti_file:
     reader = csv.reader(opti_file)
     header = next(reader)  # Skip the header row
+    for _ in range(6):
+        next(reader)
     data = [row for row in reader]
-    start_time = float(data[0][0])
-    for row in data:
-        row[0] = int(round((float(row[0]) - start_time) * 30))
-    # Remove duplicate frames based on frame number (row[0])
-    unique_data = []
-    seen_frames = set()
-    for row in data:
-        if row[0] not in seen_frames:
-            unique_data.append(row)
-            seen_frames.add(row[0])
-    data = unique_data
 
 
 
@@ -33,24 +24,14 @@ with open(f"airporttestfiles/{file_name}"+"output.csv", 'w',newline='') as opti_
     writer = csv.writer(opti_output)
     for row in data:
         time = float(row[0])
-        x1, y1, z1 = map(float, row[1:4])
-        x2,y2,z2 = map(float, row[7:10])
+        x1, y1, z1 = map(float, row[5:8])
+        x2, y2, z2 = map(float, row[11:14])
         dx = x2 - x1
-        dy = z2 - z1
-        dz = y2 - y1        
-
-
-        rx1,ry1,rz1 = map(float, row[4:7])
-        rx2,ry2,rz2 = map(float, row[10:13])
-        # board_rotation = np.array([rx2, rz2,ry2])
-        # phone_rotation = np.array([rx1, rz1,ry1 ])
-        #Skulle gerne konvertere til rotation matrix, sammenligne dem og konvertere til vektor igen
-        # R_board, _ = cv2.Rodrigues(board_rotation)
-        # R_phone, _ = cv2.Rodrigues(phone_rotation)
-        # R_relative = R_board.T @ R_phone.T
-        # relative_rotation_vector, _ = cv2.Rodrigues(R_relative)
-        # r = relative_rotation_vector.flatten()
-        writer.writerow([time, dx, dy, dz, rx1, ry1,rz1])
+        dy = y2 - y1
+        dz = z2 - z1
+        rx1,ry1,rz1 = map(float, row[2:5])
+        rx2,ry2,rz2 = map(float, row[8:11])
+        writer.writerow([time, dx, dy, dz, rx2,ry2,rz2])
 
 
 
