da325ce3393b748e9f2c136db1c409f553ba4ce3
Thamsen <casper@thamsen.dk>
2025-06-01 12:58:19 +0200

    added fig

--- kode/compare.py
+++ kode/compare.py
@@ -18,9 +18,8 @@ file_name2 = f"csvfiles\\" + selected + "pose_transformed.csv"
 val1 = np.loadtxt(file_name1, delimiter=',',skiprows=7)  # OptiTrack
 # file_name2 = r"csvfiles\experiment_005pose_transformed.csv"
 val2 = np.loadtxt(file_name2, delimiter=',')
-val2_del = val2[val2[:,7] == 10]
+val2_del = val2[val2[:,7] == 0]
 
-offset += 100
 
 # Convert rvecs (columns 4, 5, 6) in val2_del and val1 to euler angles using rotationfix.py
 # rvecs_pose = val2_del[:, 4:7]
@@ -35,9 +34,9 @@ offset += 100
 
 fig, ax = plt.subplots()
 # pose
-# ax.plot(val2_del[:,0] * scale + offset, val2_del[:,1], '.r',   markersize=2, label='pose rx')
-# ax.plot(val2_del[:,0] * scale + offset, val2_del[:,2], '.g',markersize=2, label='pose ry')
-# ax.plot(val2_del[:,0] * scale + offset, val2_del[:,3], '.b',markersize=2, label='pose rz')
+ax.plot(val2_del[:,0] * scale + offset, val2_del[:,1], '.r',   markersize=2, label='pose rx')
+ax.plot(val2_del[:,0] * scale + offset, val2_del[:,2], '.g',markersize=2, label='pose ry')
+ax.plot(val2_del[:,0] * scale + offset, val2_del[:,3], '.b',markersize=2, label='pose rz')
 # ax.plot(val2_del[:,0] * scale + offset, -np.unwrap(val2_del[:,4])/180*np.pi-np.pi, 'm', label='pose rx')
 # ax.plot(val2_del[:,0] * scale + offset, val2_del[:,5]/180*np.pi+np.pi/2, 'y', label='pose ry')
 # ax.plot(val2_del[:,0] * scale + offset, val2_del[:,6]/180*np.pi-np.pi/2, 'c', label='pose rz')
@@ -48,15 +47,18 @@ fig, ax = plt.subplots()
 
 #opti
 
+
+ax.plot(val1[:,0], val1[:,1], '-r',markersize=1, label='opti x')
+ax.plot(val1[:,0], val1[:,2], '-g',markersize=1, label='opti y')
+ax.plot(val1[:,0], val1[:,3], '-b',markersize=1, label='opti z')
 # ax.plot(val1[:,0], val1[:,4]/180*np.pi, '.m', label='opti rx')
 # ax.plot(val1[:,0], val1[:,5]/180*np.pi, '.c',label='opti rz')
 # ax.plot(val1[:,0], val1[:, 6]/180*np.pi, '.y',label='opti ry')
-# ax.plot(val1[:,0], val1[:,1], '-r',markersize=1, label='opti x')
-# ax.plot(val1[:,0], val1[:,2], '-g',markersize=1, label='opti y')
-# ax.plot(val1[:,0], val1[:,3], '-b',markersize=1, label='opti z')
-# plt.savefig(r"C:\Users\caspe\Workspace\Bachelor\datapictures\whaaaat.png")
+ax.set_xlabel('Frame (t) scaled to 240 Hz')
+ax.set_ylabel('Position (m)')
 plt.legend()
-# plt.savefig("Aruco0_e2.png")
+plt.savefig("Aruco0.png")
+# plt.savefig("nfold10.png")
 #x,y,z (1,2,3) passer med x,y,z(11,12,13)
 
 
